0f59d6de1d9040d9cf9df1ce3154f72f65a369e1
root <root@localhost.localdomain>
2016-01-02 19:33:46 -0800

    Open sql connection, open ssh connection

--- network_param_extract.py
+++ network_param_extract.py
@@ -164,4 +164,140 @@ try:
 except KeyboardInterrupt:
     print Fore.RED + "\n\n* Program aborted by user. Exiting...\n"
     sys.exit()
+
+check_sql=True
+def sql_connection(command,values):
+	global check_sql
+
+	#Define SQL Parameters
+    selected_sql_file = open(sql_file, 'r')
+    
+    #Starting from the beginning of the file
+    selected_sql_file.seek(0)
+
+    sql_host = selected_sql_file.readlines()[0].split(',')[0]
+    
+    #Starting from the beginning of the file
+    selected_sql_file.seek(0)
+    
+    sql_username = selected_sql_file.readlines()[0].split(',')[1]
+    
+    #Starting from the beginning of the file
+    selected_sql_file.seek(0)
+    
+    sql_password = selected_sql_file.readlines()[0].split(',')[2]
+    
+    #Starting from the beginning of the file
+    selected_sql_file.seek(0)
+    
+    sql_database = selected_sql_file.readlines()[0].split(',')[3].rstrip("\n")
+    
+    #Connecting and writing to database
+    try:
+        sql_conn = mdb.connect(sql_host, sql_username, sql_password, sql_database)
+    
+        cursor = sql_conn.cursor()
+    
+        cursor.execute("USE NetMon")
+        
+        cursor.execute(command, values)
+        
+        #Commit changes
+        sql_conn.commit()
+        
+    except mdb.Error, e:
+        sql_log_file = open("SQL_Error_Log.txt", "a")
+        
+        #Print any SQL errors to the error log file
+        print >>sql_log_file, str(datetime.datetime.now()) + ": Error %d: %s" % (e.args[0],e.args[1])
+        
+        #Closing sql log file:    
+        sql_log_file.close()
+        
+        #Setting check_sql flag to False if any sql error occurs
+        check_sql = False
+                
+    #Closing the sql file
+    selected_sql_file.close()
+#Initialize the necessary lists and dictionaries
+cpu_values = []
+io_mem_values = []
+proc_mem_values = []
+upint_values = []
+
+top3_cpu = {}
+top3_io_mem = {}
+top3_proc_mem = {}
+top3_upint = {}
+
+#Open SSHv2 connection to devices
+def open_ssh_conn(ip):
+    global check_sql
+    
+    #Change exception message
+    try:
+        #Define SSH parameters
+        selected_user_file = open(user_file, 'r')
+        
+        #Starting from the beginning of the file
+        selected_user_file.seek(0)
+		
+		#Reading the username from the file
+        username = selected_user_file.readlines()[0].split(',')[0]
+        
+        #Starting from the beginning of the file
+        selected_user_file.seek(0)
+        
+		#Reading the password from the file
+        password = selected_user_file.readlines()[0].split(',')[1].rstrip("\n")
+        
+        #Logging into device
+        session = paramiko.SSHClient()
+        
+        #For testing purposes, this allows auto-accepting unknown host keys
+        #Do not use in production! The default would be RejectPolicy
+        session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        
+        #Connect to the device using username and password          
+        session.connect(ip, username = username, password = password)
+        
+        #Start an interactive shell session on the router
+        connection = session.invoke_shell()	
+        
+        #Setting terminal length for entire output - disable pagination
+        connection.send("terminal length 0\n")
+        time.sleep(1)
+        
+        
+        #Reading commands from within the script
+        #Using the "\" line continuation character for better readability of the commands to be sent
+        selected_cisco_commands = """show version | include (, Version|uptime is|bytes of memory|Hz)&\
+                                  show inventory&\
+                                  show interfaces | include bia&\
+                                  show processes cpu | include CPU utilization&\
+                                  show memory statistics&\
+                                  show ip int brief | include (Ethernet|Serial)&\
+                                  show cdp neighbors detail | include Device ID&\
+                                  show ip protocols | include Routing Protocol"""
+        
+        #Splitting commands by the "&" character
+        command_list = selected_cisco_commands.split("&")
+        
+        #Writing each line in the command string to the device
+        for each_line in command_list:
+            connection.send(each_line + '\n')
+            time.sleep(3)
+        
+        #Closing the user file
+        selected_user_file.close()
+        
+        #Checking command output for IOS syntax errors
+        output = connection.recv(65535)
+        
+        if re.search(r"% Invalid input detected at", output):
+            print Fore.RED + "* There was at least one IOS syntax error on device %s" % ip
+            
+        else:
+            print Fore.GREEN + "* All parameters were extracted from device %s" % ip,
+           
  
